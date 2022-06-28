5514a67cc6df395631b3c830d8c96d5b7469194a
Enzo Almada <enzoalmada30@gmail.com>
2022-06-28 15:08:52 -0300

    Se agrega apartados de crear usuario y crear categoria

--- ProyectoBlogApp/urls.py
+++ ProyectoBlogApp/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', inicio, name="inicio"),
     path('crear-post/', crear_post, name='crear-post'),
     path('buscar-post/', buscar_post, name='buscar-post'),
+    path('crear-usuario/',crear_usuario, name="crear-usuario"),
+    path('crear-categoria/',crear_categoria, name="crear-categoria"),
 ]
--- ProyectoBlogApp/views.py
+++ ProyectoBlogApp/views.py
@@ -27,6 +27,38 @@ def crear_post(request):
     else:
         return render(request,"ProyectoBlogApp/crear_post.html",{})
 
+
+def crear_usuario(request):
+    if request.method == "POST":
+
+        info_formulario= request.POST
+        
+        usuario= Usuario(nombre = info_formulario["nombre"],apellido = info_formulario["apellido"],
+        username = info_formulario["username"],email = info_formulario["email"])
+
+        usuario.save()
+
+        return redirect("inicio")
+        
+    #GET y otros
+    else:
+        return render(request,"ProyectoBlogApp/crear_usuario.html",{})
+
+def crear_categoria(request):
+    if request.method == "POST":
+
+        info_formulario= request.POST
+        
+        categoria= Categoria(nombre = info_formulario["categoria"])
+
+        categoria.save()
+
+        return redirect("inicio")
+        
+    #GET y otros
+    else:
+        return render(request,"ProyectoBlogApp/crear_categoria.html",{})
+
 def buscar_post(request):
 
 
