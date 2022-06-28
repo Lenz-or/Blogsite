3519cf66ef91f51f7a9920f874541abf987dcb83
Enzo Almada <enzoalmada30@gmail.com>
2022-06-28 14:46:53 -0300

    se agrego buscar post

--- ProyectoBlogApp/urls.py
+++ ProyectoBlogApp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     # URLS de ProyectoCoderApp
     path('', inicio, name="inicio"),
     path('crear-post/', crear_post, name='crear-post'),
+    path('buscar-post/', buscar_post, name='buscar-post'),
 ]
--- ProyectoBlogApp/views.py
+++ ProyectoBlogApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.db.models import Q
 
 from .models import Post,Usuario,Categoria
 
@@ -25,3 +26,31 @@ def crear_post(request):
     #GET y otros
     else:
         return render(request,"ProyectoBlogApp/crear_post.html",{})
+
+def buscar_post(request):
+
+
+
+   if request.method == "POST":
+
+
+
+       post = request.POST["titulo"]
+      
+       titulos = Post.objects.filter( Q(titulo__icontains=post) ).values()
+
+
+
+       return render(request,"ProyectoBlogApp/buscar_post.html",{"titulos":titulos})
+
+
+
+   else: # get y otros
+
+
+
+        titulos =  []  #Curso.objects.all()
+      
+        return render(request,"ProyectoBlogApp/buscar_post.html",{"titulos":titulos})
+
+
